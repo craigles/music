8949dc3298cb22a8ac40cb798d501552a1fe036e
Craig Curtis <craig.d.curtis@gmail.com>
2018-08-10 16:31:14 +1000

    Create _makeindex.sh

---
+++ _recordings/_makeindex.sh
@@ -0,0 +1,2 @@
+ls | egrep '(mp3|mp4)' | \\ #filter the content you want in the index
+perl -e 'print "<html><body><ul>"; while(<>) { chop $_; print "<li><a href=\"./$_\">$_</a></li>";} print "</ul></body></html>"' > index.html
